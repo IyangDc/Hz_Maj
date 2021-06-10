89f00af3dd35efa956c0fa29ed58fdee1e5955ef
IyangDc <Launcherc@hotmail.com>
2021-06-10 18:59:55 +0800

    添加简单胡牌判定、优化player对象（增加用户字典、牌字典、牌名字典）

--- maj_core.py
+++ maj_core.py
@@ -1,7 +1,9 @@
 import random
 from maj_player import *
+player_name_list = ["Player1","Player2","Player3","Player4"]
 class Maj:
     def __init__(self,player):
+        self.player_name = ["Player1","Player2","Player3","Player4"]
         self.unfetch_list=[]
         self.Maj_dict = {
             1:"一条"
@@ -10,8 +12,8 @@ class Maj:
         self.player = player
         self.shuffle()
         for i in range(13):
-            for player_num in range(4):
-                self.player.draw(player_num,self.unfetch_list.pop(0))
+            for player_name in self.player_name:
+                self.player.draw(player_name,self.unfetch_list.pop(0))
         #self.player.show()
 
     #洗牌
@@ -21,42 +23,110 @@ class Maj:
         self.unfetch_list.sort(key=lambda num:num[1])
 
     # 发牌
-    def deal_card(self,player_num):
-        print("Player {} draw card: {}".format(player_num,self.player.draw(player_num,self.unfetch_list.pop(0))))
+    def deal_card(self,player_name):
+        print("{} draw card: {}".format(player_name,self.player.draw(player_name,self.unfetch_list.pop(0))))
         
     # 接受出牌
-    def receive_card(self,player_num,card):
+    def receive_card(self,player_name,card):
         self.received_list.append(card)
-        print("Player {} play card: {}".format(player_num,card))
+        print("Receive {}'s card: {}".format(player_name,card))
 
     # 亮牌
-    def declear_card(self,player_num):
-        print("Player {} has card:".format(player_num))
-        for card in self.player.player_list[player_num]:
-            print(card[0],end=" ")
+    def declear_card(self,player_name):
+        print("{} has card:".format(player_name))
+        for card in self.player.player_dict[player_name]['Card_list']:
+            print(card[2]+": "+str(card[0]),end=" ")
         print("")
 
     # 显示收到的牌
     def declear_received_card(self):
         print("Received card:")
+        print("",end="|")
         for card in self.received_list:
-            print(card,end=" ")
+            print(card[2],end="|")
+            #print(str(self.player.Card_Name[card]),end=" ")
+        
         print("")
+    
+    # 胡
+    def check_win(self,player_name):
+        Wall_dict = {}
+        NUM = 0
+        # 统计各种牌的张数
+        for card in self.player.player_dict[player_name]["Card_list"]:
+            if card[2] in Wall_dict:
+                Wall_dict[card[2]]+=1
+            else:
+                Wall_dict[card[2]]=1
+
+        for card_type1 in Wall_dict:
+            # 找对子
+            if Wall_dict[card_type1]==2:
+                # 找刻子
+                for card_type2 in Wall_dict:
+                    if Wall_dict[card_type2]==3:
+                        NUM += 1
+                found_flag = False
+                found_sec_flag = False
+                Firstchar = "A"
+                Numchar = "1"
+
+                #找顺子
+                for card_type3,num in Wall_dict.items():
+                    if num == 1:
+                        if found_flag:
+                            if (Firstchar == card_type3[0]) & (str(int(Numchar) + 1) == card_type3[-1]):
+                                #找到顺子第二张
+                                found_sec_flag = True
+                                pass
+                            elif (Firstchar is card_type3[0]) & (str(int(Numchar) + 2 )is card_type3[-1]) & found_sec_flag:
+                                #找到顺子第三张
+                                #计数
+                                NUM += 1
+                                #准备找下一个顺子
+                                found_flag = False
+                                found_sec_flag = False
+                            else:
+                                #顺子被杂牌断了，重置
+                                found_flag = False
+                                found_sec_flag = False
+                        else:
+                            Numchar = card_type3[-1]
+                            if Numchar.isdigit():#不是风牌、龙牌
+                                Firstchar = card_type3[0]
+                                found_flag = True
+                            else:#是风牌、龙牌
+                                found_flag = False
+                    else:#顺子被对子、刻子断了，重新开始找
+                        Firstchar = "A"
+                        Numchar = "1"
+                        found_flag = False
+                if NUM == 4:
+                    return 1
+        return 0
+        
+        
+
 b = player()
 game = Maj(b)
 while True:
-    for player_num in range(4):
-        game.declear_card(player_num)
-        game.deal_card(player_num)
-        game.declear_card(player_num)
+    for player_name in player_name_list:
+        print("---------------------------game------------------------------")
+        game.declear_card(player_name)
+        game.deal_card(player_name)
+        game.declear_card(player_name)
         # 起过一张牌之后，判断有没有胡
+        if game.check_win(player_name)==0:
+            pass
+        else:
+            print("{} win.".format(player_name))
         while True:
-            card = input("Player {} wants to play card : ".format(player_num))
-            ret,CARD = b.play(player_num,int(card))
+            card = input("{} wants to play card : ".format(player_name))
+            ret,CARD = b.discard(player_name,int(card))
             if ret == 0:
-                game.receive_card(player_num,CARD)
+                game.receive_card(player_name,CARD)
                 #打出一张牌后判断是否有人要碰、吃、杠
-                game.declear_received_card()
+                #game.declear_received_card()
                 break
-            
-        game.declear_card(player_num)
+        game.declear_card(player_name)
+        game.declear_received_card()
--- maj_player.py
+++ maj_player.py
@@ -1,21 +1,74 @@
 class player:
     def __init__(self):
-        self.player_list = []
+        self.player_wall = {"Bamboo1":0,"Bamboo2":0,"Bamboo3":0,"Bamboo4":0,"Bamboo5":0,"Bamboo6":0,"Bamboo7":0,"Bamboo8":0,"Bamboo9":0,
+                            "Dot1":0,"Dot2":0,"Dot3":0,"Dot4":0,"Dot5":0,"Dot6":0,"Dot7":0,"Dot8":0,"Dot9":0,
+                            "Character1":0,"Character2":0,"Character3":0,"Character4":0,"Character5":0,"Character6":0,"Character7":0,"Character8":0,"Character9":0,}
+        self.Card_Name  =  ["Bamboo1","Bamboo1","Bamboo1","Bamboo1",
+                            "Bamboo2","Bamboo2","Bamboo2","Bamboo2",
+                            "Bamboo3","Bamboo3","Bamboo3","Bamboo3",
+                            "Bamboo4","Bamboo4","Bamboo4","Bamboo4",
+                            "Bamboo5","Bamboo5","Bamboo5","Bamboo5",
+                            "Bamboo6","Bamboo6","Bamboo6","Bamboo6",
+                            "Bamboo7","Bamboo7","Bamboo7","Bamboo7",
+                            "Bamboo8","Bamboo8","Bamboo8","Bamboo8",
+                            "Bamboo9","Bamboo9","Bamboo9","Bamboo9",
+                            "Dot1","Dot1","Dot1","Dot1",
+                            "Dot2","Dot2","Dot2","Dot2",
+                            "Dot3","Dot3","Dot3","Dot3",
+                            "Dot4","Dot4","Dot4","Dot4",
+                            "Dot5","Dot5","Dot5","Dot5",
+                            "Dot6","Dot6","Dot6","Dot6",
+                            "Dot7","Dot7","Dot7","Dot7",
+                            "Dot8","Dot8","Dot8","Dot8",
+                            "Dot9","Dot9","Dot9","Dot9",
+                            "Character1","Character1","Character1","Character1",
+                            "Character2","Character2","Character2","Character2",
+                            "Character3","Character3","Character3","Character3",
+                            "Character4","Character4","Character4","Character4",
+                            "Character5","Character5","Character5","Character5",
+                            "Character6","Character6","Character6","Character6",
+                            "Character7","Character7","Character7","Character7",
+                            "Character8","Character8","Character8","Character8",
+                            "Character9","Character9","Character9","Character9",
+                            "EastWind","EastWind","EastWind","EastWind",
+                            "WestWind","WestWind","WestWind","WestWind",
+                            "SouthWind","SouthWind","SouthWind","SouthWind",
+                            "NorthWind","NorthWind","NorthWind","NorthWind",
+                            "RedDragon","RedDragon","RedDragon","RedDragon",
+                            "GreenDragon","GreenDragon","GreenDragon","GreenDragon",
+                            "WhiteDragon","WhiteDragon","WhiteDragon","WhiteDragon"]
+        #self.player_list = []
+        self.player_dict = {"Player1":{},"Player2":{},"Player3":{},"Player4":{}}
         #庄家
         self.dealer=0
-        for i in range(4):
-            self.player_list.append([])
-    def draw(self,player_num,card):
-        self.player_list[player_num].append(card)
-        self.player_list[player_num].sort()
+        for player in self.player_dict:
+            self.player_dict[player].update({"Card_list":[],"Wall":{}})
+        
+ #       for i in range(4):
+ #           self.player_list.append([])
+    # 起牌
+    def draw(self,player_name,card):
+        card.append(self.Card_Name[card[0]])
+        self.player_dict[player_name]['Card_list'].append(card)
+        self.player_dict[player_name]['Card_list'].sort()
         return card
-    def play(self,player_num,card):
-        for i,CARD in enumerate(self.player_list[player_num]):
+    # 出牌
+    def discard(self,player_name,card):
+        for i,CARD in enumerate(self.player_dict[player_name]['Card_list']):    
             if CARD[0] - card==0:
-                return 0,self.player_list[player_num].pop(i)
-        print("Player {} doesn't have this card!".format(player_num))
+                return 0,self.player_dict[player_name]['Card_list'].pop(i)
+        print("{} doesn't have this card!".format(player_name))
         return 1,1
+    """
+    # 输出手牌
     def show(self):
         for i,player in enumerate(self.player_list):
             print("player {}:".format(i+1))
             print(player)
+    
+    def sort_card(self,player_num):
+        player_card_dict = {}
+        for card in self.player_list[player_num]:
+            player_card_dict = {}
+    """
+    
